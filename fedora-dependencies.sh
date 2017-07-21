c01715c3bd2306893fa3919cbbd06d992ead0053
SilverEzhik <i@ezhik.me>
2017-07-20 21:42:42 -0400

    Add Fedora dependencies

---
+++ fedora-dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dnf install cairo-devel cairo-devel pam-devel libev-devel libxcb-devel libXext-devel libxkbcommon-devel libxkbcommon-x11-devel libxkbfile-devel libXpm-devel xcb-util-keysyms-devel xcb-util-image-devel xcb-util-devel
